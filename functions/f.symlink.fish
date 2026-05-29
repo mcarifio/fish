2a03041b714ea91a07f8b52818d4f5442576071b
Mike Carifio <mike@carif.io>
2026-05-29 16:49:47 -0400

    fix f.symlink

--- functions/f.symlink.fish
+++ functions/f.symlink.fish
@@ -2,7 +2,9 @@ function f.symlink -a pathname --description 'f.symlink $pathname $name... # sym
     test -f $pathname; or ret "pathname $pathname not found"
     set --local pathname $(abspath $pathname)
     set --local pathnamed (path dirname $pathname)
-    ln --symbolic --target-directory=$pathnamed $pathname $argv[2..] &> /dev/null; or true
+    for l in $argv[2..]
+        ln --symbolic --relative $pathname $pathnamed/$l.fish &> /dev/null; or true
+    end
 end
 
 
